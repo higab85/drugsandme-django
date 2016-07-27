7b57540e9000cdaa5c8dc441e13e33a6f8920e41
gebs <hive@dyn905-114.eduroam.ic.ac.uk>
2016-07-27 14:44:54 +0200

    changed all "drugs and me" collapse buttons to grey on hover

--- article/static/article/js/main.js
+++ article/static/article/js/main.js
@@ -1,7 +1,35 @@
 
 var $animationSpeed = 500;
 
+
 $(document).ready(function(){
+
+  $("#title-drugs").hover(function() {
+      $('#title-drugs-button').animate({
+        marginTop:'+=9px',
+        marginLeft:'+=9px'
+      }, $animationSpeed);
+      $("#title-drugs-collapse").removeClass("hidden");
+      $("#title-drugs-collapse").animate({
+        opacity: 1
+      }, $animationSpeed);
+      return false;
+    },function(){
+
+      $( this ).removeClass( "anim" );
+      $('#title-drugs-button').animate({
+        marginTop:'-=9px',
+        marginLeft:'-=9px'
+      }, $animationSpeed);
+      $("#title-drugs-collapse").animate({
+        opacity: 0
+      }, $animationSpeed, function(){
+        $("#title-drugs-collapse").addClass("hidden");
+        return false;
+      });
+      return false;
+  });
+
   $("#title-me").hover(function(){
       $('#title-me-button').animate({
         marginTop:'+=9px',
@@ -11,6 +39,7 @@ $(document).ready(function(){
       $("#title-me-collapse").animate({
         opacity: 1
       }, $animationSpeed);
+      return false;
     },function(){
       $('#title-me-button').animate({
         marginTop:'-=9px',
@@ -20,38 +49,9 @@ $(document).ready(function(){
         opacity: 0
       }, $animationSpeed, function(){
         $("#title-me-collapse").addClass("hidden");
+        return false;
     });
+    return false;
   });
-
-  $("#title-drugs").hover(function(){
-      $('#title-drugs-button').animate({
-        marginTop:'+=9px',
-        marginLeft:'+=9px'
-      }, $animationSpeed);
-      $("#title-drugs-collapse").removeClass("hidden");
-      $("#title-drugs-collapse").animate({
-        opacity: 1
-      }, $animationSpeed);
-    },function(){
-      $('#title-drugs-button').animate({
-        marginTop:'-=9px',
-        marginLeft:'-=9px'
-      }, $animationSpeed);
-      $("#title-drugs-collapse").animate({
-        opacity: 0
-      }, $animationSpeed, function(){
-        $("#title-drugs-collapse").addClass("hidden");
-      });
-  });
-
-  // $(".title-collapse div").hover(function(){
-  //   var $backgroundColor = $(this).css("background-color");
-  //   console.log("light: " + $backgroundColor + ", dark: " + $(this).darken() );
-  //   console.log(this);
-  //   $(this).animate({
-  //     opacity: 1,
-  //     backgroundColor: "rgb(105, 171, 176)"
-  //   }, $animationSpeed);
-  // }, function(){});
 
 });
